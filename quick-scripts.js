467cf204593646c2a1137bb9515c2d8b6f05b43f
Jessica <jesi.hester@gmail.com>
2016-07-07 11:47:46 -0700

    Added clarification

--- quick-scripts.js
+++ quick-scripts.js
@@ -2,22 +2,25 @@
 * Quick Accordion
 */
 /*
-* .clickMe is a span, next to a div; when clicked, the div will be opened
+* Info: .clickMe is a span, next to a div; when clicked, the div will be opened
 * Ideally, these elements will be wrapped in an <li> or <div> in order to 
 * be their only siblings.
+*
 * Use: <li class="faq-question">
 *			<span class="clickMe">{@question}</span>
 *			<div class="faq-answer panel">
 *				{@answer}
 *			</div>
 *		</li>
+*
 */
 $(".clickMe").on("click", function(){$(this).next("div").slideToggle();});
 
 /*
 * Quick Dynamic Copyright Year
 * 
-* To use, simply add a span with the ID of #copyYear in your footer.
+* Info: To use, simply add a span with the ID of #copyYear in your footer.
+*
 * Use: <span class="info">&copy; <span id="copyYear"></span> by {@company}. </span>
 */
 var currentYear = (new Date).getFullYear();
